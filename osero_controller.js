bce2d23dcd20a7c6934c1d506d4fbf0a4d897b40
karenhasegawa <85094840+karenhasegawa@users.noreply.github.com>
2021-06-14 02:54:50 +0900

    commit_6/14
    線型

--- osero_controller.js
+++ osero_controller.js
@@ -16,7 +16,8 @@ window.addEventListener('DOMContentLoaded',function(){
         newElement.style.height = "70px";
         newElement.style.margin = "1px 1px 1px 1px";
         newElement.style.float = "left";
-        parentDiv.style.margin = "6px 6px 6px 6px";
+        newElement.style.position="relative";
+        newElement.style.display="block";
 
     //駒の作成
         //----------要素----------//
@@ -24,12 +25,25 @@ window.addEventListener('DOMContentLoaded',function(){
         let pieace = 'pieace';
         newPieace.setAttribute("class",pieace);
         let parentClass = document.getElementById(name);
-        console.log(parentClass);
         parentClass.appendChild(newPieace);
+        //css->main.css
 
 
+    } 
 
+    //初期設定完了
 
+    setDisc(8,8,1);
 
-    }   
+    //(x,y)のマスに石を置く
+    //d=0:石を消す
+    //d=1:黒石を置く
+    //d=2:白石を置く
+    function setDisc(x,y,d){
+        let p = y * 8 + x;//2次元座標を1次元座標に変換
+        console.log(p);
+
+    }
+    
+    
 });
